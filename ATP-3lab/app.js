7b2666583595ab98e8c75167d98d884e413ea2c8
amaitpaul2255 <55456860+amaitpaul2255@users.noreply.github.com>
2019-10-23 12:41:14 +0600

    Add files via upload

---
+++ ATP-3lab/app.js
@@ -0,0 +1,26 @@
+//DECLARATION
+var express = require('express');
+var bodyParser = require('body-parser');
+var expSession = require('express-session');
+var cookieParser = require('cookie-parser');
+var ejs = require('ejs');
+
+
+//CONFIGURATION
+app.set('view engine', 'ejs');
+
+//MIDDLEWARE
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(expSession({secret:'my top secret value', saveUninitialized:true, resave: false}));
+app.use(cookieParser());
+
+
+//ROUTER
+app.get('/', function(request, response){
+	response.send('index page!');
+});
+
+//SERVER STARTUP
+app.listen(3000, function(){
+	console.log('server started at 3000...');
+});
